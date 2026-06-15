e2761668f494b542a602a242dc0d8ff34979fd84
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2026-06-15 13:22:39 +0530

    setup internal version variable

---
+++ internal/version/version.go
@@ -0,0 +1,19 @@
+package version
+
+import "runtime/debug"
+
+// Build-time parameters set via -ldflags
+var Version = "unknown"
+
+func init() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	mainVersion := info.Main.Version
+	if mainVersion == "" || mainVersion == "(devel)" {
+		return
+	}
+	// If the binary is built using `go install`
+	Version = mainVersion
+}
